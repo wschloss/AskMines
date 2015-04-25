b8f595f9707a82276c209966d71804a324d2ee53
Michael Kuzminsky <mkuzmins@mines.edu>
2015-04-25 13:06:15 -0600

    Fixed sorting, now sorts by upvotes, still working on sorting by answers, also sorts by tags as well

--- finalproj/app/controllers/questions_controller.rb
+++ finalproj/app/controllers/questions_controller.rb
@@ -37,20 +37,20 @@ class QuestionsController < ApplicationController
     if(@foundqs.size <= 0) 
 	   Question.all.each do |q|
 		#Check the content of the question
-		if(q.content.include? @content)
+		if(q.content.downcase.include? @content)
 			@foundqs.push(q)		
 		end	
 
 		#Check the title as well
-		if(q.title.include? @content and not @foundqs.include? q)
+		if(q.title.downcase.include? @content and not @foundqs.include? q)
 			@foundqs.push(q)
 		end
     	end
     end
 
     #Then, Sort by upvotes first, then by answers
-    @foundqs.sort_by{|q| [q[:upvotes]]}#, q.answers.size]}#.reverse!()
-    @foundqs.reverse!.reverse!
+    @foundqs.sort_by!{|q| q[:upvotes]}#, q.answers.size]}#.reverse!()
+    @foundqs.reverse!
 
     # @questions = search_by_content(@content)
   end
